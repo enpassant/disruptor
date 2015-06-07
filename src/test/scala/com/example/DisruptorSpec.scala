1a44fa9f64849e2f665a1819149a36dd344bc222
Kálmán Ferenc <kalman.ferenc@akribis.hu>
2015-06-07 10:51:50 +0200

    Test disruptor receiving event and process

--- src/test/scala/com/example/DisruptorSpec.scala
+++ src/test/scala/com/example/DisruptorSpec.scala
@@ -3,7 +3,7 @@ package com.example
 import akka.actor.ActorSystem
 import akka.actor.Actor
 import akka.actor.Props
-import akka.testkit.{ EventFilter, TestActorRef, TestActors, TestKit, ImplicitSender }
+import akka.testkit.{ EventFilter, TestActorRef, TestActors, TestKit, TestProbe, ImplicitSender }
 import org.scalatest.WordSpecLike
 import org.scalatest.Matchers
 import org.scalatest.BeforeAndAfterAll
@@ -25,6 +25,12 @@ class DisruptorSpec(_system: ActorSystem) extends TestKit(_system) with Implicit
     "ordering Consumers" in {
       val disruptor = system.actorOf(Disruptor.props)
 
+      def checkOrder(expectResult: Array[Disruptor.Consumer]) = {
+          disruptor ! Disruptor.GetState
+          val result = receiveOne(10.millis)
+          result should be(expectResult)
+      }
+
       val consumer1 = Disruptor.Consumer(1, "/user/PingActor1")
       val consumer2 = Disruptor.Consumer(2, "/user/PingActor2")
       val consumer3 = Disruptor.Consumer(3, "/user/PingActor3")
@@ -33,15 +39,38 @@ class DisruptorSpec(_system: ActorSystem) extends TestKit(_system) with Implicit
       val consumer6 = Disruptor.Consumer(6, "/user/PingActor6")
 
       disruptor ! consumer6
-      disruptor ! consumer3
-      disruptor ! consumer4
-      disruptor ! consumer2
-      disruptor ! consumer1
-      disruptor ! consumer5
+      checkOrder(Array(Disruptor.Consumer(0, ""), consumer6))
 
-      disruptor ! Disruptor.GetState
-      val result = receiveOne(10.millis)
-      result should be(Array(Disruptor.Consumer(0, ""), consumer1, consumer2, consumer3, consumer4, consumer5, consumer6))
+      disruptor ! consumer3
+      checkOrder(Array(Disruptor.Consumer(0, ""), consumer3, consumer6))
+
+      disruptor ! consumer4
+      checkOrder(Array(Disruptor.Consumer(0, ""), consumer3, consumer4, consumer6))
+
+      disruptor ! consumer2
+      checkOrder(Array(Disruptor.Consumer(0, ""), consumer2, consumer3, consumer4, consumer6))
+
+      disruptor ! consumer1
+      checkOrder(Array(Disruptor.Consumer(0, ""), consumer1, consumer2, consumer3, consumer4, consumer6))
+
+      disruptor ! consumer5
+      checkOrder(Array(Disruptor.Consumer(0, ""), consumer1, consumer2, consumer3, consumer4, consumer5, consumer6))
+    }
+  }
+
+  "A Disruptor actor" must {
+    "receive an event and send to process" in {
+      val disruptor = system.actorOf(Disruptor.props)
+
+      val probe1 = TestProbe()
+      val path1 = "/" + (probe1.ref.path.elements mkString "/")
+      disruptor ! Disruptor.Consumer(6, path1)
+
+      disruptor ! Disruptor.Initialized
+      val data = "Test"
+      disruptor ! Disruptor.Event(data)
+
+      probe1.expectMsg(500.millis, Disruptor.Process(path1, data))
     }
   }
 
