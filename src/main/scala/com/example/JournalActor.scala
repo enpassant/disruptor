8bdb012d8fa541128b747c6884b4870d1d6e35ae
Kálmán Ferenc <kalman.ferenc@akribis.hu>
2016-07-16 12:07:42 +0200

    Fix JournalActor counter calculation

--- src/main/scala/com/example/JournalActor.scala
+++ src/main/scala/com/example/JournalActor.scala
@@ -11,7 +11,7 @@ class JournalActor(journaler: Journaler) extends Actor with ActorLogging {
   import JournalActor._
 
   val random = new scala.util.Random
-  var counter = 0L
+  var counter = 1L
 
   val serialization = SerializationExtension(context.system)
   val db = journaler.init(serialization)
@@ -23,9 +23,9 @@ class JournalActor(journaler: Journaler) extends Actor with ActorLogging {
     case ReplayNext(disruptor) =>
       actor ! ReplayNext(disruptor)
 
-    case Process(0, index, id, command) =>
+    case Process(seqNr, index, id, command: Command) =>
       log.debug("In JournalActor - process command")
-      sender ! Processed(index, id, command)
+      sender ! Processed(index, id, None)
 
     case Process(seqNr, index, id, Array(others @ _*)) =>
       val (replayed, remains) = others.filter {
@@ -34,10 +34,13 @@ class JournalActor(journaler: Journaler) extends Actor with ActorLogging {
         case Replayed(elem) => true
         case _ => false
       }
-      if (replayed.size > 0) sender ! Processed(index - remains.size, id, replayed)
+      if (replayed.size > 0) {
+        counter += replayed.size
+        sender ! Processed(index - remains.size, id, replayed)
+      }
       log.debug("In JournalActor - Replayed: {}", replayed mkString ", ")
       log.debug("In JournalActor - Remains: {}", remains mkString ", ")
-      if (remains.size > 0) process(Process(seqNr, index, id, remains))
+      if (remains.size > 0) process(Process(seqNr + replayed.size, index, id, remains))
 
     case Processed(index, id, Replayed(data)) =>
       actor ! ReplayNext(disruptor)
@@ -49,7 +52,6 @@ class JournalActor(journaler: Journaler) extends Actor with ActorLogging {
     case Processed(index, id, data) =>
       log.debug("In JournalActor - received process message: {}, {}, {}",
         index, counter, data)
-      counter += 1
 
     case msg =>
       process(msg)
@@ -67,23 +69,25 @@ class JournalActor(journaler: Journaler) extends Actor with ActorLogging {
 
     case Process(seqNr, index, id, Replayed(data)) =>
       log.debug("In JournalActor - Replayed: {}", data)
-      sender ! Processed(index, id, Replayed(data))
+      counter += 1
+      sender ! Processed(index, id, None)
 
     case Process(seqNr, index, id, data: Seq[AnyRef @unchecked]) =>
       log.debug("In JournalActor - received process message: {}, {}", index, data)
       val serializer = serialization.findSerializerFor(data)
 //      var i = index - data.size
       db.writeSeqData(seqNr, data)
+      if (counter != seqNr) log.info("Key is invalid. {} vs {}", seqNr, counter)
       counter += data.length
-      sender ! Processed(index, id, data)
+      sender ! Processed(index, id, None)
 
     case Process(seqNr, index, id, data) =>
       log.debug("In JournalActor save {}", seqNr)
       log.debug("In JournalActor - received process message: {}, {}", index, data)
       db.writeData(seqNr, data)
+      if (counter != seqNr) log.info("Key 2 is invalid. {} vs {}", seqNr, counter)
       counter += 1
-      if (counter != seqNr) log.info("Key 2 is invalid. {} vs {}", index, counter)
-      sender ! Processed(index, id, data)
+      sender ! Processed(index, id, None)
 
     case msg =>
       log.debug("In JournalActor - received message: {}", msg)
